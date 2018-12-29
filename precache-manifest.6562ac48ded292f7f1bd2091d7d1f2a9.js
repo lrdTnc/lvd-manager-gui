eeb91832e05081bbe4f1a60ef5bc20ae9eeeb602
lrdTnc <antoniu.ienciu@outlook.com>
2018-12-29 18:11:03 +0200

    Updates

---
+++ precache-manifest.6562ac48ded292f7f1bd2091d7d1f2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27964d0217c07531ca9",
+    "url": "/lvd-manager-gui/static/js/runtime~main.c27964d0.js"
+  },
+  {
+    "revision": "47b4f3129372c00ade45",
+    "url": "/lvd-manager-gui/static/js/main.47b4f312.chunk.js"
+  },
+  {
+    "revision": "3111df933e46680e1ef6",
+    "url": "/lvd-manager-gui/static/js/1.3111df93.chunk.js"
+  },
+  {
+    "revision": "47b4f3129372c00ade45",
+    "url": "/lvd-manager-gui/static/css/main.e9f1379b.chunk.css"
+  },
+  {
+    "revision": "3111df933e46680e1ef6",
+    "url": "/lvd-manager-gui/static/css/1.943377c9.chunk.css"
+  },
+  {
+    "revision": "af3f75cd71b24b08df914dac0a6a5d32",
+    "url": "/lvd-manager-gui/index.html"
+  }
+];
