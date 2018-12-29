5bce056900bbcc1dba4c7c5df7cf8bb27b0ec71f
lrdTnc <antoniu.ienciu@outlook.com>
2018-12-29 17:57:42 +0200

    Initial GUI Build

---
+++ docs/precache-manifest.3e5f7f82e01d2292e43560574d4a0638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10251e850ec49b26eb8d",
+    "url": "/static/js/main.10251e85.chunk.js"
+  },
+  {
+    "revision": "3111df933e46680e1ef6",
+    "url": "/static/js/1.3111df93.chunk.js"
+  },
+  {
+    "revision": "10251e850ec49b26eb8d",
+    "url": "/static/css/main.e9f1379b.chunk.css"
+  },
+  {
+    "revision": "3111df933e46680e1ef6",
+    "url": "/static/css/1.943377c9.chunk.css"
+  },
+  {
+    "revision": "8df6e4ad0478489902a756efa19f0997",
+    "url": "/index.html"
+  }
+];
